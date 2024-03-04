ac6ea7cab39b586e3d99c91377265b722e343a6c
Gabriel Rodrigues <69880032+gabrielvpina@users.noreply.github.com>
2024-03-04 15:09:15 -0300

    Update fastq_download_input.py - exclusão de pastas .sra

--- fastq_download_input.py
+++ fastq_download_input.py
@@ -1,26 +1,38 @@
+# Essa versão permite a exclusão das pastas dos arquivos .sra de forma automatizada
 import subprocess
+import shutil
+import os
 
-# This script allows the user to download SRA libraries by their SRR id 
+# Solicitar ao usuário que insira os SRA library runs separados por espaço
 entry = input("-> Enter the SRA library runs separated by space: ")
 user_input = entry.split()
 sra_numbers = user_input
 
-user_path = input("-> Insert destination folder path here:")
+# Solicitar ao usuário que insira o caminho da pasta de destino
+user_path = input("-> Insert destination folder path here: ")
 
+# Lista para armazenar os caminhos das pastas das bibliotecas SRA
+sra_folders = []
 
-# this will download the .sra files to ~/ncbi/public/sra/ (will create directory if not present)
+# Baixar os arquivos .sra para ~/ncbi/public/sra/ (o diretório será criado se não existir)
 for sra_id in sra_numbers:
-    print ("Currently downloading: " + sra_id)
+    print("Currently downloading: " + sra_id)
     prefetch = "prefetch " + sra_id
-    print ("The command used was: " + prefetch)
+    print("The command used was: " + prefetch)
     subprocess.call(prefetch, shell=True)
 
-# this will extract the .sra files from above into a folder named 'fastq'
-for sra_id in sra_numbers:
-    print ("Generating fastq for: " + sra_id)
-    # insert destination folder path here
-    sra_file_path = user_path + sra_id + "/" + sra_id + ".sra"
+    # Adicionar o caminho da pasta da biblioteca SRA à lista
+    sra_folder_path = os.path.join(user_path, sra_id)
+    sra_folders.append(sra_folder_path)
+
+# Extrair os arquivos .sra para a pasta 'fastq'
+for sra_id, sra_folder_path in zip(sra_numbers, sra_folders):
+    print("Generating fastq for: " + sra_id)
+    sra_file_path = os.path.join(sra_folder_path, f"{sra_id}.sra")
     fastq_dump = "fastq-dump --outdir fastq --gzip --skip-technical --readids --read-filter pass --dumpbase --split-3 --clip " + sra_file_path
-    print ("The command used was: " + fastq_dump)
+    print("The command used was: " + fastq_dump)
     subprocess.call(fastq_dump, shell=True)
 
+    # Excluir a pasta da biblioteca SRA após a extração
+    shutil.rmtree(sra_folder_path)
+    print(f"Deleted folder: {sra_folder_path}")
