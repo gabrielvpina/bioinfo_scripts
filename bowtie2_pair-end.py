bc1e35377074d008d1cdd51414bc8d653c40d88a
Gabriel Rodrigues <69880032+gabrielvpina@users.noreply.github.com>
2024-03-10 01:16:39 -0300

    Update bowtie2_pair-end.py

--- bowtie2_pair-end.py
+++ bowtie2_pair-end.py
@@ -1,11 +1,3 @@
-# Example pair-end query
-
-# NECESSARIO ANTES: bowtie2-build -f input_reference.fasta index_prefix
-
-# bowtie2 -x index_prefix [-q|--qseq|-f|-r|-c] [-1 input_reads_pair_1.[fasta|fastq] -2 input_reads_pair_2.[fasta|fastq] | -U input_reads.[fasta|fastq]] -S bowtie2_alignments.sam [options]
-
-# bowtie2 -x index_prefix -f -1 input_reads_pair_1.fasta -2 input_reads_pair_2.fasta -S bowtie2_alignments.sam --local -p $SLURM_NTASKS_PER_NODE
-
 import subprocess
 import os
 
@@ -13,44 +5,33 @@ import os
 dir_name = "bowtie2"
 os.makedirs(dir_name, exist_ok=True)
 
-# Obter a lista de arquivos na pasta fastp/
-pasta_bibliotecas = input("Insert path to the .fastq.gz archives:")
+# Obter o caminho completo para a pasta fastp/
+pasta_fastp = "fastp"
 
-# Lista para armazenar os pares de arquivos das bibliotecas pair-end
-pares_bibliotecas = []
-
-# Iterar sobre os arquivos na pasta e agrupá-los em pares
-for arquivo in os.listdir(pasta_bibliotecas):
+# Iterar sobre os arquivos encontrados na pasta fastp/
+for arquivo in os.listdir(pasta_fastp):
+    # Verificar se o arquivo tem o padrão correto de nome (_1.fastq.gz)
     if arquivo.endswith("_1.fastq.gz"):
-    
-        # Verificar se há um arquivo correspondente para o par
-        arquivo_par = arquivo.replace("_1.fastq.gz", "_2.fastq.gz")
-        if arquivo_par in os.listdir(pasta_bibliotecas):
-            pares_bibliotecas.append((arquivo, arquivo_par))
-            
-# Executar o Bowtie2 para cada par de arquivos de biblioteca pair-end
-for biblioteca1, biblioteca2 in pares_bibliotecas:
-    # Nome do arquivo de saída para o alinhamento
-    saida_alinhamento = f"alinhamento_{biblioteca1[:-9]}_vs_{biblioteca2[:-10]}.fastq.gz"
+        # Extrair o nome do arquivo (sem extensão) e sem o sufixo _1
+        samp = arquivo.replace("_1.fastq.gz", "")
 
+        # Imprimir a mensagem de processamento
+        print(f"Processing sample {samp}")
 
-# Iterar sobre os arquivos na lista
-#for arquivo in lista_arquivos:
-    # Caminho de entrada e saída para o Fastp
-    #entrada1 = f"fastq/{arquivo1}"
-    #entrada2 = f"fastq/{arquivo2}"
-    #saida = f"{dir_name}/{arquivo}.fastq.gz"
+        # Verificar se existe o arquivo correspondente com o sufixo _2
+        arquivo_2 = f"{samp}_2.fastq.gz"
+        if arquivo_2 in os.listdir(pasta_fastp):
+            # Comando Bowtie2
+            comando_bowtie2 = [
+                "bowtie2", "-x", "cacao_index",
+                "-1", f"{pasta_fastp}/{arquivo}", "-2", f"{pasta_fastp}/{arquivo_2}",
+                "--un-conc", f"{dir_name}/{samp}.fq"
+            ]
 
-    # Comando Fastp
-    comando_bowtie2 = [
-        "bowtie2",
-        "-x", "cacao_index",  # Substitua "index_reference" pelo índice Bowtie2 adequado
-        "-1", os.path.join(pasta_bibliotecas, biblioteca1),
-        "-2", os.path.join(pasta_bibliotecas, biblioteca2),
-        "--un", saida_alinhamento
-    ]
+            # Executar o comando Bowtie2 usando subprocess
+            subprocess.run(comando_bowtie2)
+        else:
+            print(f"Arquivo correspondente para {arquivo} não encontrado")
 
-    # Executar o comando Fastp
-    subprocess.run(comando_bowtie2)
 
 
