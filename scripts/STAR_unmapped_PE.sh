6c95b6e220d2eeed71ec0b147cd6da489a0bc097
Gabriel Rodrigues <69880032+gabrielvpina@users.noreply.github.com>
2024-04-03 18:36:28 -0300

    Update STAR_unmapped_PE.sh

--- scripts/STAR_unmapped_PE.sh
+++ scripts/STAR_unmapped_PE.sh
@@ -4,24 +4,30 @@
 dir_name="STAR_unmapped"
 mkdir -p "$dir_name"
 
-# Obter o caminho completo para a pasta fastp/
-pasta_fastp="fastp/"
+# Descompactar arquivos da pasta fastp
+gunzip fastp/*
 
 # Iterar sobre os arquivos encontrados na pasta fastp/
-for arquivo in "$pasta_fastp"/*_1.fastq.gz; do
+for arquivo in fastp/*_1.fastq; do
     # Verificar se o arquivo tem o padrão correto de nome (_1.fastq.gz)
     if [[ -f "$arquivo" ]]; then
         # Extrair o nome do arquivo (sem extensão) e sem o sufixo _1
-        samp=$(basename "$arquivo" _1.fastq.gz)
+        samp=$(basename "$arquivo" _1.fastq)
 
         # Imprimir a mensagem de processamento
         echo "Processing sample $samp"
 
         # Verificar se existe o arquivo correspondente com o sufixo _2
-        arquivo_2="$pasta_fastp/${samp}_2.fastq.gz"
+        arquivo_2="$pasta_fastp/${samp}_2.fastq"
         if [[ -f "$arquivo_2" ]]; then
-            # Comando Bowtie2
-            STAR --runThreadN 12 --genomeDir star_genome --readFilesIn "$arquivo" "$arquivo_2" --outReadsUnmapped Fastx 
+            
+            # Comando STAR
+            STAR --runThreadN 12 --genomeDir star_genome --readFilesIn "$arquivo" "$arquivo_2" --outReadsUnmapped Fastx >> STAR_unmapped/
+
+
         fi
     fi
 done
+
+# Compactar arquivos de volta 
+gzip fastp/*
