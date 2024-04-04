493ae885b14a3bfce6215b22819e49e309e2aa5b
Gabriel Rodrigues <69880032+gabrielvpina@users.noreply.github.com>
2024-04-04 12:46:19 -0300

    Update STAR_uncompressed.sh

--- scripts/STAR_uncompressed.sh
+++ scripts/STAR_uncompressed.sh
@@ -27,7 +27,7 @@ for arquivo in "$pasta_fastp"/*_1.fastq.gz; do
         STAR --runThreadN 12 --genomeDir star_genome --readFilesIn "$pasta_fastp/${samp}_1.fastq" "$pasta_fastp/${samp}_2.fastq" --outReadsUnmapped Fastx >> "$dir_name/${samp}_unaligned.fastq"
 
         # Comprimir novamente os arquivos _1.fastq e _2.fastq
-        gzip "$pasta_fastp/${samp}_1.fastq"
-        gzip "$pasta_fastp/${samp}_2.fastq"
+        rm "$pasta_fastp/${samp}_1.fastq"
+        rm "$pasta_fastp/${samp}_2.fastq"
     fi
 done
